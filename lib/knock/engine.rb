c45148dc0078abf7e59cc0c65542ccdbafb02a43
Maros Hluska <mhluska@gmail.com>
2017-11-16 08:23:56 -0800

    Avoid loading all of lib (fixes Heroku deploy)

--- lib/knock/engine.rb
+++ lib/knock/engine.rb
@@ -1,6 +1,11 @@
 module Knock
-  class Engine < ::Rails::Engine
-    config.eager_load_paths += Dir["#{config.root}/lib/**/"]
+  class Engine < ::Rails::Engine 
+    if ::Rails.const_defined? 'Generators'
+      config.eager_load_paths += Dir["#{config.root}/lib/**/"]
+    else
+      require_relative './authenticable'
+    end
+  
     isolate_namespace Knock
   end
 end
